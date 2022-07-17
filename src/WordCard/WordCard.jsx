322f7327244e2426fa4b49e7349d2ca54aafa9aa
VeraMelli <veralitv88@mail.ru>
2022-07-17 13:23:35 +0200

    add translation

--- src/WordCard/WordCard.jsx
+++ src/WordCard/WordCard.jsx
@@ -1,10 +1,26 @@
-import React from "react";
-import s from "./WordCard.css"
+import React, {useState} from "react";
 
-function WordCard(){
-    return(
-        <div className={s.wrapper}>Word card</div>
-    )
+import s from "./WordCard.css";
+
+function WordCard() {
+
+const [pressed, setPressed] = useState(false);
+
+
+const handleChange = () =>{
+    setPressed(!pressed);
+   }
+
+  return (
+    <div className={s.wrapper}>
+      <div className={s.english}>Car</div>
+      <div className={s.transcription}>[kɑ:]</div>
+      {pressed && <div className={s.russian}>
+        Автомобиль
+      </div>}
+      <button onClick={handleChange} className={s.check__button}>Проверить</button>
+    </div>
+  );
 }
 
 export default WordCard;
